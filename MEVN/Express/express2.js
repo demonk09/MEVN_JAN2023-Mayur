644b04ca723c8deb9aa83cda5af184c972185a5d
mayur kamble <54568942+demonk09@users.noreply.github.com>
2023-02-21 20:34:28 +0530

    commit_21-02-23

---
+++ MEVN/Express/express2.js
@@ -0,0 +1,16 @@
+const express = require("express") ;
+const userService = require("./userService");
+const app = express () ;
+
+app.use(express.json()) ;
+
+app. get ("/users", async (request, response) =>{
+  try{
+          const users = await userService.getUserData() ;
+         response.status(200).json(user) ;
+      } catch (error) {
+       console.error (error) ;
+     }
+}) ;
+
+app.listen(3000) ;
