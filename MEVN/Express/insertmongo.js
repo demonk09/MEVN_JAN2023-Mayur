4dc66879dc4bd876aa4204a888aea13d121f7b82
mayur kamble <54568942+demonk09@users.noreply.github.com>
2023-03-10 20:32:46 +0530

    Commit_to_sample_project

--- MEVN/Express/insertmongo.js
+++ MEVN/Express/insertmongo.js
@@ -1,13 +1,27 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("social");
-  var myobj = { name: "Company Inc", address: "Highway 37" };
-  dbo.collection("user").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017';
+
+// Database Name
+const dbName = 'collage';
+
+// Use async/await to connect to the server
+async function connectToServer() {
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  const db = client.db('social');
+  console.log("Connected successfully to server");
+  // Do something with the database here...
+
+  // Get the collection
+  const collection = db.collection('user');
+
+  // Insert a document
+  const result = await collection.insertOne({ F_nm: 'Mayur', L_nm: 'Kamble', DOB: '09/10/2000',Phone_no: '9130386399' });
+
+  console.log(`Inserted ${result.insertedCount} document into the collection`);
+
+  client.close();
+}
+
+connectToServer();
